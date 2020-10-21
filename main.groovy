dba976f14d8ed7e84c45d5c968acfddf69d79d2d
alejocespedes <alejandrocespedesa@gmail.com>
2020-10-21 12:09:37 -0500

    ssh

--- main.groovy
+++ main.groovy
@@ -1,11 +1,27 @@
-#!/bin/bash
-
 def funcion (){
     echo "respuesta de groovy ${params.PERSON}"
     echo "respuesta de groovy ${params.NOMBRE}"
 }
 
-def remote(){
-    sshpass -p tcuser ssh docker@192.168.30.100 'docker exec -it ubuntu bash' && 'ls -lrt'
+def getRemote () {
+        def remote = [:]
+        remote.name = 'remote'
+        remote.host = "192.168.30.100"
+        remote.user = "docker"
+        remote.password = "tcuser"
+        remote.allowAnyHosts = true
+
+        return remote
+}   
+
+def comandos(){
+  println('ejecuto comando 1')
+  def remote = getRemote()
+  println('ejecuto comando 2')
+  sshCommand remote: remote, command: "docker exec -it ubuntu bash"
+  sshCommand remote: remote, command: "cd /home"
+  println('ejecuto comando')
+  sshScript remote: remote, script: "prueba.sh"
 }
+
  return this
