ff7d3a99eb7b7fd09e7ea0ba6d860d913dc9d843
alejocespedes <alejandrocespedesa@gmail.com>
2020-10-21 08:42:35 -0500

    new

--- main.groovy
+++ main.groovy
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 def funcion (){
     echo "respuesta de groovy ${params.PERSON}"
     echo "respuesta de groovy ${params.NOMBRE}"
 }
-return this
+
+def remote(){
+    sshpass -p tcuser ssh docker@192.168.30.100 'docker exec -it ubuntu bash' && 'ls -lrt'
+}
+ return this
