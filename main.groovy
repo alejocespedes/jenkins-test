f397e072cc63c5ad172582f7f9fc02015d34475a
alejocespedes <alejandrocespedesa@gmail.com>
2020-10-28 14:39:07 -0500

    test

--- main.groovy
+++ main.groovy
@@ -3,6 +3,14 @@ def funcion (){
     echo "respuesta de groovy ${params.NOMBRE}"
 }
 
+def downloadfile(){
+    sh "cd /tmp && curl -L https://github.com/alejocespedes/jenkins-test/tarball/91f8a47fa9ff9db455bf1b152c49646ccaff4a59 | tar xz"
+}
+
+def copyfiletodest(){
+    sh "curl --insecure --user docker:tcuser -T /tmp/alejocespedes-jenkins-test-91f8a47/test.groovy sftp://192.168.30.100/tmp/"
+}
+
 def getRemote () {
     withCredentials([usernamePassword(credentialsId: 'ubuntu', passwordVariable: 'password', usernameVariable: 'userName')]) {
         def remote = [:]
@@ -18,10 +26,8 @@ def getRemote () {
 
 def comandos(){
   def remote = getRemote()
-  sshCommand remote: remote, command: 'docker exec ubuntu bin/sh -c "cd /home && . ./prueba.sh"'
+  sshCommand remote: remote, command: "docker cp /tmp/test.groovy ubuntu:/tmp"
+  // sshCommand remote: remote, command: 'docker exec ubuntu bin/sh -c "cd /home && . ./prueba.sh"'
 }
 
-def downloadfile(){
-    ["git", "clone", "${params.URL}"].execute()
-}
  return this
